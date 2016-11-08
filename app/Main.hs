0b0502d31fce0694eab5dc055f0fd4ee8b8f861f
art rimbaud <rimbaudcode@gmail.com>
2016-11-08 18:34:04 +0100

    random reader implemented

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,21 @@
 module Main where
 
-import Lib
+import Paths_shams (getDataFileName)
+import System.Random (randomRIO)
+import System.FilePath.Posix ((</>),(<.>))
 
 main :: IO ()
-main = someFunc
+main = putStr =<< readFile =<< getRuleFilePath =<< genRuleNumber
+
+
+getRuleFilePath :: Show a => a -> IO FilePath
+getRuleFilePath rule = getDataFileName ("data" </> show rule <.> "txt")
+
+genRuleNumber :: IO Int
+genRuleNumber = randomRIO (minRuleNumber, maxRuleNumber)
+
+minRuleNumber :: Int
+minRuleNumber = 1
+
+maxRuleNumber :: Int
+maxRuleNumber = 40
