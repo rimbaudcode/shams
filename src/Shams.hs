8a3a5d7c6c9dfe63d71750f491a40c8f76016dcc
art rimbaud <rimbaudcode@gmail.com>
2019-03-04 09:12:46 -0500

    opt: prelude explicitly imported

--- src/Shams.hs
+++ src/Shams.hs
@@ -1,16 +1,17 @@
-
+module Shams
     ( genRuleNumber
     , getRuleFilePath
     ) where
 
 import Paths_shams           (getDataFileName)
+import Prelude               (FilePath, IO, Int, show, (.))
 import System.FilePath.Posix ((<.>), (</>))
 import System.Random         (randomRIO)
 
 genRuleNumber :: IO Int
 genRuleNumber = randomRIO (minRuleNumber, maxRuleNumber)
 
-getRuleFilePath      :: Int -> IO FilePath
+getRuleFilePath :: Int -> IO FilePath
 getRuleFilePath = getDataFileName . (<.> "txt") . ("data" </>) . show
 
 maxRuleNumber :: Int
